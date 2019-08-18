431afe370ab97eacc6706058034913ba914f8381
53JIlLenWe11 <ruin.of.messger@gmail.com>
2019-08-18 20:47:48 +0900

    zprofileとbash_profileを再度分離したのでsetupを修正

--- setup.sh
+++ setup.sh
@@ -22,8 +22,8 @@ else
   exit 1
 fi
 
-\cp config/.bash_profile_.zprofile ~/.bash_profile
-\cp config/.bash_profile_.zprofile ~/.zprofile
+\cp config/.bash_profile ~/.bash_profile
+\cp config/.zprofile ~/.zprofile
 \cp config/.bashrc ~/
 \cp config/.zshrc ~/
 \cp config/.colorrc ~/
