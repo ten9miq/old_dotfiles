6a1f8053a0c9d79d37ab4ebb6e565cb26799f75b
53JIlLenWe11 <ruin.of.messger@gmail.com>
2019-10-15 00:32:27 +0900

    実行方法を非同期に行うようにする

--- setup.sh
+++ setup.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
-echo '###############################################################################'
-echo '###     All setup execute                                                   ###'
-echo '###       use shell by bash zsh fish                                        ###'
-
 # setup時に必要な環境変数やエラートラップの実行
 source ./setup_env.sh
+logo
 # echo $OS
 # echo $PROJECT_PATH
 
-source $PROJECT_PATH/shell_common/init.sh
-source $PROJECT_PATH/tmux/init.sh
-source $PROJECT_PATH/ssh/init.sh
-source $PROJECT_PATH/bin/init.sh
-source $PROJECT_PATH/bash/init.sh
-source $PROJECT_PATH/zsh/init.sh
-source $PROJECT_PATH/git/init.sh
-# source $PROJECT_PATH/vim/init.sh
-# source $PROJECT_PATH/etc/init.sh
-# source $PROJECT_PATH/fish/init.sh
+# 並列実行できるもの
+# dists=('zsh' 'vim' 'shell_common' 'tmux' 'ssh' 'bin' 'bash' 'git' 'fish' 'etc')
+dists=('zsh' 'shell_common' 'tmux' 'ssh' 'bin' 'bash' 'git')
+for e in ${dists[@]}; do
+  for script in $PROJECT_PATH/$e/*.sh; do
+    if [ -f $script ]; then
+      run $script
+      bash $script && ok "$script" || error "$script" &
+    else
+      continue
+    fi
+  done
+done
 
+# 終了を待つ
+wait
 
+echo ''
+echo '###############################################################################'
 echo '###     COMPLITE.                                                           ###'
 echo '###############################################################################'
+exit
