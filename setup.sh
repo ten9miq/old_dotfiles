46c359fafdb0113d0df69d5c34bada9a22361c96
53JIlLenWe11 <ruin.of.messger@gmail.com>
2019-10-14 19:48:16 +0900

    binフォルダのinitを作成

--- setup.sh
+++ setup.sh
@@ -11,8 +11,13 @@ source ./setup_env.sh
 source $PROJECT_PATH/shell_common/init.sh
 source $PROJECT_PATH/tmux/init.sh
 source $PROJECT_PATH/ssh/init.sh
+source $PROJECT_PATH/bin/init.sh
 # source $PROJECT_PATH/bash/init.sh
 # source $PROJECT_PATH/zsh/init.sh
+# source $PROJECT_PATH/etc/init.sh
+# source $PROJECT_PATH/fish/init.sh
+# source $PROJECT_PATH/git/init.sh
+# source $PROJECT_PATH/vim/init.sh
 
 
 echo '###     COMPLITE.                                                           ###'
