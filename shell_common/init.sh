70adaf19a78a53d21a6ed8e98ea02ea256d427b4
53JIlLenWe11 <ruin.of.messger@gmail.com>
2019-10-14 19:18:57 +0900

    配下のファイルをすべてコピーするように変更

--- shell_common/init.sh
+++ shell_common/init.sh
@@ -6,8 +6,17 @@ if [ -z $PROJECT_PATH ]; then
   source $THIS_SCRIPT_PATH/../setup_env.sh
 fi
 
-\mkdir -p ~/.config
 # # aliasなどzshとbashで共通できるもの
-\cp -r $PROJECT_PATH/shell_common/ ~/.config/shell_common
+copy_target="$HOME/.config/shell_common"
+mkdir -p $copy_target
+# 指定のファイル以外をコピーしてかつ、配下のディレクトリをコピーする
+find $THIS_SCRIPT_PATH -type f -not -name 'init.sh' | while read -r find_path;
+do
+  # 本スクリプトのパスをfindで取得したパス文字列から削除する
+  relative_path=${find_path##$THIS_SCRIPT_PATH/}
+  # コピー先のディレクトリがなければ作成する
+  mkdir -p $copy_target/$(dirname $relative_path);
+  cp $find_path $copy_target/$relative_path;
+done
 
 echo '###     shell common init end                                               ###'
